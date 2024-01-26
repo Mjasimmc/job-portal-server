2fac4edc2ea5997e73d33d9e9001531fbc9b849d
Mjasim <mjasimmc@gmail.com>
2024-01-26 12:43:44 +0530

    fix admin routes

--- controller/authentication/jwtAuthenticator.js
+++ controller/authentication/jwtAuthenticator.js
@@ -11,8 +11,8 @@ export const validateUser = async (req,res,next)=>{
 }
 export const validateAdmin = async (req,res,next)=>{
     try {
-        // const decoded =  decodeJwtToken(req.headers.authorization)
-        // req.user = decoded
+        const decoded =  decodeJwtToken(req.headers.authorization)
+        req.user = decoded
         next()
     } catch (error) {
         res.status(401).send("user not validated")
