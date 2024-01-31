c74725d2c44fa3727918d36bc29128010d19a3e0
Mjasim <mjasimmc@gmail.com>
2024-01-31 20:47:51 +0530

    searching for error

--- controller/user/subscriptionPlan.js
+++ controller/user/subscriptionPlan.js
@@ -38,6 +38,7 @@ export const paymentRazorpay = async (req, res) => {
     try {
         const { amount, planId } = req.body
         console.log(amount,planId)
+        console.log(process.env.RAZORPAY_KEY_ID ,process.env.RAZORPAY_KEY_ID)
         const instance = new Razorpay({
             key_id: process.env.RAZORPAY_KEY_ID,
             key_secret: process.env.RAZORPAY_SECRET,
