aed825f78cd41a94ce603283095cee670bc1fd07
Mjasim <mjasimmc@gmail.com>
2024-02-01 16:15:50 +0530

    completed working

--- controller/authentication/adminAuth.js
+++ controller/authentication/adminAuth.js
@@ -1,10 +1,18 @@
 import { createJwtToken } from "./jwtAuth.js"
 
+
+const auth = {
+    email: 'admin@gmail.com',
+    password: 'Jasim123@'
+}
 export const adminLogin = async (req, res) => {
     try {
         const { email, password } = req.body
-        const token = await createJwtToken({email,password})
-        res.status(200).send({token})
+        if (email !== auth.email || password !== auth.password) {
+            return res.status(404).send("un authorized entry")
+        }
+        const token = await createJwtToken({ email, password })
+        res.status(200).send({ token })
     } catch (error) {
         res.status(500).send('internal server error')
     }
