b8783eb10755edda05039ac74b405dc71b28b381
Mjasim <mjasimmc@gmail.com>
2024-02-15 16:37:27 +0530

    added some rest fucntions sdftg

--- controller/authentication/jwtAuthenticator.js
+++ controller/authentication/jwtAuthenticator.js
@@ -1,19 +1,22 @@
 import { findUserWithUserId } from "../../dbOperation/userMangement.js"
 import { decodeJwtToken } from "./jwtAuth.js"
 
-export const validateUser = async (req,res,next)=>{
+export const validateUser = async (req, res, next) => {
     try {
-        const decoded =  decodeJwtToken(req.headers.authorization)
-        const user = await findUserWithUserId(decoded._id)
-        if(!user){
-            throw "user not found"
+        const decoded = decodeJwtToken(req.headers.authorization);
+        const user = await findUserWithUserId(decoded._id);
+        if (!user) {
+            return res.status(404).send("user not found");
         }
-        req.user = user
-        next()
+        if (user.block) {
+            return res.status(403).send("User is blocked.");
+        }
+        req.user = user;
+        next();
     } catch (error) {
-        res.status(401).send("user not validated")
+        res.status(401).send("User not validated");
     }
-}
+};
 export const validateAdmin = async (req,res,next)=>{
     try {
         const decoded =  decodeJwtToken(req.headers.authorization)
